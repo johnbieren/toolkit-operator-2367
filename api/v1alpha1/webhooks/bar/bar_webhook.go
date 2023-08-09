04a36f671189998b67683f78e82676b8f6b6298c
Troy <tbarrett@redhat.com>
2023-08-09 12:47:37 +0100

    Add loader

--- api/v1alpha1/webhooks/bar/bar_webhook.go
+++ api/v1alpha1/webhooks/bar/bar_webhook.go
@@ -2,30 +2,34 @@ package webhooks
 
 import (
 	"context"
-	"github/troy/sample-operator/api/v1alpha1"
+	"fmt"
 
 	"github.com/go-logr/logr"
+	"github.com/redhat-appstudio/operator-toolkit-example/loader"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/redhat-appstudio/operator-toolkit-example/api/v1alpha1"
 )
 
 // Webhook describes the data structure for the author webhook
 type Webhook struct {
 	client client.Client
+	loader loader.ObjectLoader
 	log    logr.Logger
 }
 
 // Register registers the webhook with the passed manager and log.
 func (w *Webhook) Register(mgr ctrl.Manager, log *logr.Logger) error {
 	w.client = mgr.GetClient()
+	w.loader = loader.NewLoader()
 	w.log = log.WithName("bar")
 
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(&v1alpha1.Bar{}).
 		WithDefaulter(w).
-		//WithValidator(w).
 		Complete()
 
 }
@@ -34,13 +38,21 @@ type WebhookInterface interface {
 	Register(mgr ctrl.Manager, log *logr.Logger) error
 }
 
-// Default implements webhook.Defaulter so a webhook will be registered for the type.
 func (w *Webhook) Default(ctx context.Context, obj runtime.Object) error {
 	return nil
 }
 
+// +kubebuilder:webhook:path=/validate-appstudio-redhat-com-v1alpha1-bar,mutating=false,failurePolicy=fail,sideEffects=None,groups=appstudio.redhat.com,resources=bars,verbs=create;update,versions=v1alpha1,name=vbar.kb.io,admissionReviewVersions=v1
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type.
 func (w *Webhook) ValidateCreate(ctx context.Context, obj runtime.Object) error {
+	bar := obj.(*v1alpha1.Bar)
+
+	_, err := w.loader.GetFoo(ctx, w.client, bar.Spec.Foo, bar.Namespace)
+	if err != nil {
+		return fmt.Errorf("resource references an unexistent Foo resource (%s/%s)", bar.Namespace, bar.Spec.Foo)
+	}
+
 	return nil
 }
 
---
+++ loader/loader.go
@@ -0,0 +1,40 @@
+package loader
+
+import (
+	"context"
+
+	"github.com/redhat-appstudio/operator-toolkit-example/api/v1alpha1"
+	toolkit "github.com/redhat-appstudio/operator-toolkit/loader"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type ObjectLoader interface {
+	GetBars(ctx context.Context, cli client.Client, foo *v1alpha1.Foo) ([]v1alpha1.Bar, error)
+	GetFoo(ctx context.Context, cli client.Client, name, namespace string) (*v1alpha1.Foo, error)
+}
+
+type loader struct{}
+
+func NewLoader() ObjectLoader {
+	return &loader{}
+}
+
+// GetBars loads the list of Bar resources associated with the Foo resource passed as a parameter.
+func (l *loader) GetBars(ctx context.Context, cli client.Client, foo *v1alpha1.Foo) ([]v1alpha1.Bar, error) {
+	bars := &v1alpha1.BarList{}
+
+	err := cli.List(ctx, bars,
+		client.InNamespace(foo.Namespace),
+		client.MatchingFields{"spec.foo": foo.Name})
+	if err != nil {
+		return nil, err
+	}
+
+	return bars.Items, nil
+}
+
+// GetFoo returns the Foo resource with the given name and namespace.
+func (l *loader) GetFoo(ctx context.Context, cli client.Client, name, namespace string) (*v1alpha1.Foo, error) {
+	foo := &v1alpha1.Foo{}
+	return foo, toolkit.GetObject(name, namespace, cli, ctx, foo)
+}
